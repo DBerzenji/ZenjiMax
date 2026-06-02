6640232b21699f83adac6af8bad99b17bad2f4c9
DBerzenji <98441386+DBerzenji@users.noreply.github.com>
2026-06-02 05:14:14 +0000

    Cloning from ZenjiMini's Max Branch

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ZenjiMax
+Cloned from https://github.com/DBerzenji/zenjimini/tree/ZenjiMax
+
+ZenjiMini was great for simple users, but I wanted more complex features and forcasting.
---
+++ sw.js
@@ -0,0 +1,40 @@
+console.log('SW FILE LOADED');
+
+const CACHE_NAME = 'zenji-cache-v1';
+
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+      console.log('INSTALL EVENT');
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
